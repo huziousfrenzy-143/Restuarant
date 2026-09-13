9712b583093fa6075b6d128b88625941b459be58
arhamsiaf65 <arhamsaif65@gmail.com>
2026-09-14 01:05:04 +0500

    fix: update super admin email in seed.sql

--- infra/seed.sql
+++ infra/seed.sql
@@ -5,5 +5,5 @@
 
 INSERT INTO public.super_admin_users (id, name, email, password_hash)
 VALUES
-('usr-super-admin', 'Super Admin Operator', 'arhamsaifofficial@gmail.com', '11be4f46ffea4a56a698cebcfbbca9ad3108c4eeb2ea24eec3bf5c4d054e70e9a11756543b5cecf3ab3eb04d2be7d47ec57dcead22a0a2df3aa8d4512e0220d9')
-ON CONFLICT (id) DO UPDATE SET password_hash = EXCLUDED.password_hash;
+('usr-super-admin', 'Super Admin Operator', 'huziouzfrenzy@gmail.com', '11be4f46ffea4a56a698cebcfbbca9ad3108c4eeb2ea24eec3bf5c4d054e70e9a11756543b5cecf3ab3eb04d2be7d47ec57dcead22a0a2df3aa8d4512e0220d9')
+ON CONFLICT (id) DO UPDATE SET password_hash = EXCLUDED.password_hash, email = EXCLUDED.email;
